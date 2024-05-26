95dfa0fccf8511d97f1e5394c1a96dc4aa9a528a
Miguel Negron <manegron@redborder.com>
2024-05-26 23:28:11 +0100

    [WIP] Adapt metadata.rb and add README.md

--- resources/metadata.rb
+++ resources/metadata.rb
@@ -6,5 +6,5 @@ description      'Installs/Configures cookbook-rb-selinux'
 issues_url       'https://github.com/redBorder/cookbook-rb-selinux/issues' if respond_to?(:issues_url)
 source_url       'https://github.com/redBorder/cookbook-rb-selinux' if respond_to?(:source_url)
 chef_version     '>= 15.0' if respond_to?(:chef_version)
-supports         'rocky linux', '>= 9.0'
+supports         'rocky', '>= 9.0'
 version          '0.1.1'
